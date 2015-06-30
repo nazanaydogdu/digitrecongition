07bba5fb0067cf6c3a3f00ecb0f5de779ce0df42
nazanaydogdu <nazanaydogdu@gmail.com>
2015-06-30 12:45:45 +0300

    Update digitrecognizer_PCA_SVM.R
    Added comments & explanations

--- digitrecognizer_PCA_SVM.R
+++ digitrecognizer_PCA_SVM.R
@@ -1,35 +1,31 @@
-#Example for the kaggle forums.
+#Digit Recognition on kaggle.com
+#SCORE: 0.98300
 library(stats)
 library("e1071")
 library("devtools")
 library("ggbiplot")
 
-trainingSet <- read.csv("data/train.csv")
+trainingSet <- read.csv("data/train.csv")   #read data files
 validationSet <- read.csv("data/test.csv")
-#combinedSet <- read.csv("data/combined.csv")
 set.seed(20140202)
-#trainingSet <- train[sample(1:nrow(train), N), ]
-#validationSet <- train[sample(25000:nrow(train), M), ]
-#validationSet <- train[!(rownames(train) %in% rownames(trainingSet)), ]
 
-trainingLabel <- trainingSet[, 1]
-trainingSet <- trainingSet[, -1]
-#validationLabel <- validationSet[, 1]
-#validationSet <- validationSet[, -1]
+
+trainingLabel <- trainingSet[, 1]          #read labels
+trainingSet <- trainingSet[, -1]           #read data without labels
 
 prc <- proc.time()
-principalComps <- prcomp( ~. , data = trainingSet)
+principalComps <- prcomp( ~. , data = trainingSet)  #retrieve PCs
 screeplot(principalComps, type="line", main="Principle Components")
 num.of.comp.train = 50
 pca.train.rotation <- principalComps$rotation
 pca.train.rotation <- pca.train.rotation[,1:num.of.comp.train]
-trainingPRC <- as.matrix(trainingSet) %*% pca.train.rotation
-validationPRC <- as.matrix(validationSet) %*% pca.train.rotation
+trainingPRC <- as.matrix(trainingSet) %*% pca.train.rotation  #apply PCs to training data
+validationPRC <- as.matrix(validationSet) %*% pca.train.rotation  #apply PCs to validation data
 
-train.model <- svm(trainingPRC, trainingLabel,  type = "C-classification", kernel = "radial", gamma=0.01, cost = 10)
+train.model <- svm(trainingPRC, trainingLabel,  type = "C-classification", kernel = "radial", gamma=0.01, cost = 10)  #train SVM
 
-svm.pred <- predict(train.model, validationPRC)
-write.csv(as.matrix(svm.pred), file = "calculateddata/results.csv")
+svm.pred <- predict(train.model, validationPRC)  #predict labels for validation data
+write.csv(as.matrix(svm.pred), file = "calculateddata/results.csv")  #write predictions into CVS file
 print(proc.time() - prc)
 #sum(svm.pred == validationLabel)/length(validationLabel)
 
